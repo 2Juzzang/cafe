bee7c7fae0282c2014c868d757d7e220f3183fee
JiHoon <skyy630@naver.com>
2022-01-17 16:19:34 +0900

    feat. marker, data add

--- src/Page/Map.js
+++ src/Page/Map.js
@@ -1,21 +1,25 @@
 /* global kakao */
 import React, { useEffect } from 'react';
+import { markerData } from '../Data/MarkerData';
 import { Grid } from '../elements/index';
 const Map = () => {
   useEffect(() => {
     let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     let options = {
       //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 5, //지도의 레벨(확대, 축소 정도)
+      center: new kakao.maps.LatLng(35.828557, 127.128853), //지도의 중심좌표.
+      level: 8, //지도의 레벨(확대, 축소 정도)
     };
 
     let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-    let markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
-    let marker = new kakao.maps.Marker({
-      position: markerPosition,
+
+    markerData.forEach((el) => {
+      new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(el.lat, el.long),
+        title: el.title,
+      });
     });
-    marker.setMap(map);
   }, []);
 
   return (
